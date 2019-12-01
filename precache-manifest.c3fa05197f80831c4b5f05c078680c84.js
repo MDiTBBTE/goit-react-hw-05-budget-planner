191d8b464d9a9a18347398196f3dd36f576cb4bf
mditbbte <andrii.ds.kor@gmail.com>
2019-12-01 21:02:40 +0200

    Updates

---
+++ precache-manifest.c3fa05197f80831c4b5f05c078680c84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84cf93523240aafa576fc7f320a4db75",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "3a419c8cf46a7cf9536c",
+    "url": "/goit-react-hw-05-budget-planner/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "0bbfdadb84c1ceb386d3",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.67e7994e.chunk.css"
+  },
+  {
+    "revision": "3a419c8cf46a7cf9536c",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.7ba72f53.chunk.js"
+  },
+  {
+    "revision": "0bbfdadb84c1ceb386d3",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.db40e00e.chunk.js"
+  },
+  {
+    "revision": "4ed4565f60dd2ea96ff0",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime-main.704d8471.js"
+  }
+]);
